b415b1a4740406bd5dcfbee90a68efa69a6ff649
PyPylia <me@pypylia.dev>
2022-12-12 12:41:31 +1100

    Day 2 finished

--- src/day_2/main.rs
+++ src/day_2/main.rs
@@ -21,7 +21,7 @@ impl Actions {
         }
     }
 
-    fn get_score(self, opponent: &Actions) -> u32 {
+    fn get_score(&self, opponent: &Actions) -> u32 {
         match self {
             Actions::Rock => match opponent {
                 Actions::Rock => 3,
@@ -34,24 +34,48 @@ impl Actions {
                 Actions::Scissors => 0,
             },
             Actions::Scissors => match opponent {
-                Actions::Rock => 6,
-                Actions::Paper => 3,
-                Actions::Scissors => 0,
+                Actions::Rock => 0,
+                Actions::Paper => 6,
+                Actions::Scissors => 3,
             },
         }
     }
 
-    fn get_value(self: &Actions) -> u32 {
+    fn get_value(&self) -> u32 {
         match self {
             Actions::Rock => 1,
             Actions::Paper => 2,
             Actions::Scissors => 3,
         }
     }
+
+    fn get_action(&self, opponent: &Actions) -> Actions {
+        match self {
+            Actions::Rock => match opponent {
+                // LOSE
+                Actions::Rock => Actions::Scissors,
+                Actions::Paper => Actions::Rock,
+                Actions::Scissors => Actions::Paper,
+            },
+            Actions::Paper => match opponent {
+                // DRAW
+                Actions::Rock => Actions::Rock,
+                Actions::Paper => Actions::Paper,
+                Actions::Scissors => Actions::Scissors,
+            },
+            Actions::Scissors => match opponent {
+                // WIN
+                Actions::Rock => Actions::Paper,
+                Actions::Paper => Actions::Scissors,
+                Actions::Scissors => Actions::Rock,
+            },
+        }
+    }
 }
 
 fn main() -> anyhow::Result<()> {
-    let mut total_score: u32 = 0;
+    let mut action_total_score: u32 = 0;
+    let mut outcome_total_score: u32 = 0;
 
     for line in INPUT.lines() {
         let chars: Vec<&str> = line.trim().split(" ").collect();
@@ -59,10 +83,22 @@ fn main() -> anyhow::Result<()> {
         let opponent = Actions::from_char(chars[0]);
         let you = Actions::from_char(chars[1]);
 
-        total_score += you.get_value() + you.get_score(&opponent);
+        action_total_score += you.get_value() + you.get_score(&opponent);
+
+        let you = you.get_action(&opponent);
+
+        outcome_total_score += you.get_value() + you.get_score(&opponent);
     }
 
-    println!("Total score: {}", total_score);
+    println!(
+        "Action total score: {}",
+        action_total_score
+    );
+
+    println!(
+        "Outcome total score: {}",
+        outcome_total_score
+    );
 
     Ok(())
 }
